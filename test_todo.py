47d0e76ed754097ccfc7514ea6e0cb8f11105879
jon-b-alves <alves.b.jon@gmail.com>
2025-04-23 18:35:08 -0400

    removed unneeded test

--- test_todo.py
+++ test_todo.py
@@ -96,23 +96,6 @@ def test_store_task(tmp_path):
     assert updated_tasks[3]["task"] == "D"
 
 
-def test_show_tasks(capsys, tmp_path):
-    file = tmp_path / "test.json"
-    tasks = [
-        {"id": 1, "task": "A", "completed": False},
-        {"id": 3, "task": "B", "completed": True},
-        {"id": 5, "task": "C", "completed": False},
-    ]
-    with file.open("w") as f:
-        json.dump(tasks, f, indent=4)
-
-    todo.show_tasks(file)
-    captured = capsys.readouterr()
-    assert "(1) A" in captured.out
-    assert "(3) B" in captured.out
-    assert "(5) C" in captured.out
-
-
 def test_clear_tasks(tmp_path):
     file = tmp_path / "test.json"
     tasks = [
